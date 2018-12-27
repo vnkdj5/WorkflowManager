9782e237d3012481aff5ca3f6fbbc3cc8b5a04ea
Akshay Laxman Ubale <akshayubale10051997@gmail.com>
2018-12-27 13:09:33 +0530

    repo for components

---
+++ src/main/java/com/workflow/bean/ComponentRepository.java
@@ -0,0 +1,60 @@
+package com.workflow.bean;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.annotations.Beta;
+import com.workflow.annotation.wfComponent;
+
+@Component
+public class ComponentRepository {
+	private static ComponentRepository mComponentRepository=null;
+	
+	public ArrayList<String> components;
+	
+	
+	private ComponentRepository() {
+		components = new ArrayList<>();
+		components.add("Start");
+		components.add("End");
+		
+		//addition of @component annotation and reading all component classes
+		ClassPathScanningCandidateComponentProvider scanner =
+				new ClassPathScanningCandidateComponentProvider(true);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(wfComponent.class));
+		for (BeanDefinition bd : scanner.findCandidateComponents("com.workflow.component")) {
+			try {
+				Class<?> cls = Class.forName(bd.getBeanClassName().toString());
+				System.out.println(cls.isAnnotationPresent(wfComponent.class));
+				if(cls.getAnnotation(wfComponent.class).complete()) {
+					components.add(bd.getBeanClassName().replace("com.workflow.component.", ""));
+				}
+				
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	//initilization or contextaware
+	
+	@PostConstruct
+	public static ComponentRepository getInstance(){
+		if(mComponentRepository==null) {
+			
+			mComponentRepository = new ComponentRepository();
+		}
+		
+		return mComponentRepository;
+	}
+}
