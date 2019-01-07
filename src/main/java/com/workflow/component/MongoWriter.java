2a1d94174ff7ac9e20d6158ac56524623e1900d1
Akshay Laxman Ubale <akshayubale10051997@gmail.com>
2019-01-07 16:06:15 +0530

    component fix1

--- src/main/java/com/workflow/component/MongoWriter.java
+++ src/main/java/com/workflow/component/MongoWriter.java
@@ -8,12 +8,21 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.*;
 import com.workflow.annotation.wfComponent;
 
+import javax.xml.crypto.Data;
+import java.util.HashMap;
+
 @wfComponent(complete=true)
 public class MongoWriter implements Component{
 
 	Entity input;
 	Entity output;
-	
+
+	String Username;
+	String Password;
+	String Database;
+	String Collection;
+	String Url;
+
 	MongoClient mongo;
 	MongoDatabase db;
 	MongoCollection<Document> collection;
@@ -28,9 +37,9 @@ public class MongoWriter implements Component{
 
 	@Override
 	public boolean init(Entity config) {
-		mongo = new MongoClient((String)config.getObjectByName("url"),27017);
-		db = mongo.getDatabase((String)config.getObjectByName("database"));
-		collection = db.getCollection((String)config.getObjectByName("collection"));
+		mongo = new MongoClient(Url,27017);
+		db = mongo.getDatabase(Database);
+		collection = db.getCollection(Collection);
 		return true;
 	}
 
@@ -40,6 +49,14 @@ public class MongoWriter implements Component{
 		String Configform = "{\"schema\":{\"type\":\"object\",\"title\":\"Comment\",\"properties\":{\"name\":{\"title\":\"Username\",\"type\":\"string\",\"required\":true},\"password\":{\"title\":\"Password\",\"type\":\"string\",\"required\":true},\"collection\":{\"title\":\"collection_name\",\"type\":\"string\",\"required\":true},\"database\":{\"title\":\"database_name\",\"type\":\"string\",\"required\":true},\"url\":{\"title\":\"Url\",\"type\":\"string\",\"required\":true}},\"required\":[\"name\",\"password\",\"collection\",\"database\",\"url\"]},\"form\":[\"name\",\"password\",\"collection\",\"database\",\"url\",{\"type\":\"submit\",\"style\":\"btn-info\",\"title\":\"OK\"},{\"type\":\"button\",\"style\":\"btn-info testConBtn\",\"title\":\"Test\",\"onClick\":\"testConn(myForm)\"}]}";
 		Entity config = new Entity();
 		config.addKeyValue("FORM", Configform);
+		HashMap<String,String> model = new HashMap<>();
+		model.put("name",Username);
+		model.put("password",Password);
+		model.put("url",Url);
+		model.put("collection",Collection);
+		model.put("database",Database);
+
+		config.addKeyValue("MODEL",model);
 		return config;
 	}
 
@@ -59,6 +76,7 @@ public class MongoWriter implements Component{
 	@Override
 	public void setInput(Entity input) {
 		// TODO Auto-generated method stub
+		input = new Entity();
 		input.addKeyValue("INPUT", input.getObjectByName("OUTPUT"));
 	}
 
@@ -72,6 +90,11 @@ public class MongoWriter implements Component{
 	@Override
 	public void setConfig(Entity config) {
 		// TODO Auto-generated method stub
+		Username = (String)config.getObjectByName("username");
+		Password = (String)config.getObjectByName("password");
+		Database = (String)config.getObjectByName("database");
+		Collection = (String)config.getObjectByName("collection");
+		Url = (String)config.getObjectByName("url");
 		init(config);
 		setOutput(null);
 	}
