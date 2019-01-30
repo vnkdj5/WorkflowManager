9d76027d761e2997666d3e93f89e3fb9e90b7444
Akshay Laxman Ubale <akshayubale10051997@gmail.com>
2019-01-30 16:52:26 +0530

    mongoquery fix 3

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -472,7 +472,16 @@ app.controller('DiagramCtrl', ['$scope', '$rootScope', 'fileUpload', 'graphServi
         componentService.getConfig(WFId, componentKey).then(
             function success(response) {
                 $scope.model = {};
-                let curForm = response.data.FORM;//JSON.parse(response.data.FORM);
+                let curForm = JSON.parse(response.data.FORM,function (key, value) {
+                    if (value && (typeof value === 'string') && value.indexOf("function") === 0) {
+                        // we can only pass a function as string in JSON ==> doing a real function
+                        var jsFunc = new Function('return ' + value)();
+                        return jsFunc;
+
+                    }
+
+                    return value;
+                });//JSON.parse(response.data.FORM);
 
                 for (let i in curForm.form) {//For assigning WFId and compId (for specializing API)
                     if (curForm.form[i].endpoint) {
