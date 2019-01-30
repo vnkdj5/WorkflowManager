98b4ce507e1fc8032a4688e4ce338b3295bdb4bf
Akshay Laxman Ubale <akshayubale10051997@gmail.com>
2019-01-30 14:12:55 +0530

    code gen 1

--- src/main/java/com/workflow/controller/CodeGenerationController.java
+++ src/main/java/com/workflow/controller/CodeGenerationController.java
@@ -4,23 +4,16 @@ import java.util.HashMap;
 
 import com.workflow.service.Helper;
 import com.workflow.service.runmanager.DefaultRunManager;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.workflow.bean.LogicGraph;
-import com.workflow.service.CodeGenerationService;
-import com.workflow.service.GraphService;
 
 @RestController
 public class CodeGenerationController {
-	
-	@Autowired
-	CodeGenerationService codeGenerationService;
+
 
 	@Autowired
 	Helper helper;
@@ -33,7 +26,7 @@ public class CodeGenerationController {
 		if((boolean)map.get("error")) {
 			return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
 		}else {
-			//codeGenerationService.generateCode((LogicGraph)map.get("nodeList"));
+
 			new DefaultRunManager().run((LogicGraph) map.get("nodeList"),null);
 		}
 		
