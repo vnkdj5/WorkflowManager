eb37ffc8483f3ac259fe1f970181e6110fc6725f
Vaibhav Kumbhar <vnkdj5@gmail.com>
2019-02-04 16:27:13 +0530

    MongoQuery js function at backend added

--- src/main/webapp/scripts/controllers/formController.js
+++ src/main/webapp/scripts/controllers/formController.js
@@ -35,20 +35,4 @@ app.controller('FormController',['$scope', '$q','$http', function($scope,$q,$htt
 	$scope.model = {};
 	
 }]);
-app .config(['$translateProvider', function($translateProvider) {
-	// Simply register translation table as object hash
-	$translateProvider.translations('en', {
-		'modules.upload.dndNotSupported': 'Drag n drop not surpported by your browser',
-		'modules.attribute.fields.required.caption': 'Required',
-		'modules.upload.descriptionSinglefile': 'Drop your file here',
-		'modules.upload.descriptionMultifile': 'Drop your file(s) here',
-		'buttons.add': 'Open file browser',
-		'modules.upload.field.filename': 'Filename',
-		'modules.upload.field.preview': 'Preview',
-		'modules.upload.multiFileUpload': 'Multifile upload',
-		'modules.upload.field.progress': 'Progress',
-		'buttons.upload': 'Upload'
-	});
-	$translateProvider.preferredLanguage('en');
 
-}]);
